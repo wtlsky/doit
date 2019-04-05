c3ed1ee65f02cd7e40b60bfbc7cdd9c662bbe5f7
wtl <964817166@qq.com>
2019-04-05 17:10:52 +0800

    完成控制条样式

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <app-control></app-control>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import AppControl from '@/components/AppControl.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    AppControl
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/AppControl.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="app-control">
+      <div class="iconfont icon-code logo"></div>
+      <div class="control-bar">
+          <div class="btn iconfont icon-mini" title="迷你模式"></div>
+          <div class="btn iconfont icon-hide" title="最小化"></div>
+          <div class="btn iconfont icon-max" title="最大化"></div>
+          <div class="btn iconfont icon-close" title="关闭"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {}
+};
+</script>
+<style lang='scss' scoped>
+.app-control {
+  -webkit-app-region: drag;
+  background: #36C2CF;
+  height: 50px;
+  line-height: 50px;
+  padding: 0 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.logo{
+    font-size: 30px;
+    color: #FFF;
+}
+.btn{
+    display: inline-block;
+    color: #FFF;
+    opacity: 0.8;
+    margin: 0 5px;
+    font-size: 18px;
+    &:hover {
+        opacity: 1;
+        cursor: pointer;
+    }
+}
+</style>
